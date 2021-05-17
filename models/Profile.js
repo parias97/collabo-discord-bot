463b6d47b240438e7e0f1669f8ca32ead5125493
Peter Arias <peter.arias97@gmail.com>
2021-05-17 13:00:05 -0400

    Added mongodb

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const {Schema, model} = require('mongoose');
 
 const Profile = Schema({
     id: {
@@ -20,5 +19,4 @@ const Profile = Schema({
     }
 });
 
-let profile = mongoose.model('profile', profile);
-module.exports = profile;
+module.exports = model("Profile", Profile);
